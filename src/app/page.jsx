210ff45b59cc61473a14924ee3424329ac243b0a
Marlon Diego <105674967+MarlonLupaca@users.noreply.github.com>
2025-04-07 23:14:21 -0500

    Inicializacion del frontend

---
+++ src/app/page.jsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="bg-gray-700 text-[#eee] font-bold">
+      <span>Inicializacion del frontend</span>
+    </div>
+  );
+}
