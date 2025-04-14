f7083485e0e7234393f348a218de8ac9f89ecfc9
roke741 <jhordieroque741@gmail.com>
2025-04-14 00:58:18 -0500

    feat: add Logo component for branding display

---
+++ src/components/header/logo.jsx
@@ -0,0 +1,12 @@
+export default function Logo() {
+  return (
+    <>
+      <p className="h-[30px]">
+        <span className="font-[700] text-primary">V</span>erde
+      </p>
+      <p>
+        <span className="font-[700] text-secondary">R</span>aíz
+      </p>
+    </>
+  );
+}
