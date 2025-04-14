f4b99cad2d7708246d92fe7834eaa1c01fab2f77
roke741 <jhordieroque741@gmail.com>
2025-04-14 00:59:07 -0500

    refactor: remove unused font import and simplify Header component styling

--- src/app/Components/Header.jsx
+++ src/app/Components/Header.jsx
@@ -1,21 +1,14 @@
-import { Montserrat_Alternates } from 'next/font/google'
 import { FaRegUser } from "react-icons/fa";
 import { IoCartOutline } from "react-icons/io5";
 import React from 'react'
 import Link from 'next/link';
 
-const montserrat = Montserrat_Alternates({
-    subsets: ['latin'],
-    weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
-    style: ['normal', 'italic'],
-    display: 'swap',
-})
 
 const Header = () => {
     return (
         <div className='w-full flex text-text justify-around relative h-[12vh] items-center z-[5] bg-background shadow-xl/2'>
 
-            <div className={`${montserrat.className} text-text font-[500] text-[30px]  pl-6 flex flex-col justify-center w-[310px]`}>
+            <div className={`text-text font-[500] text-[30px]  pl-6 flex flex-col justify-center w-[310px]`}>
                 <Link href="/" className='sw-fit cursor-pointer'>
                     <p className='h-[30px]'><span className="font-[700] text-primary">V</span>erde</p>
                     <p><span className="font-[700] text-secondary">R</span>aíz</p> 
@@ -24,7 +17,7 @@ const Header = () => {
 
 
             <nav className='flex items-center  px-10 py-2 rounded-full'>
-                <ul className={`${montserrat.className} font-[600] text-text text-[16px] tracking-wide flex gap-8`}>
+                <ul className={`font-[600] text-text text-[16px] tracking-wide flex gap-8`}>
                     <li className='transition-all ease-out duration-300 hover:scale-105 hover:text-hover-text'>
                         <Link href="/products">Productos</Link>
                     </li>
