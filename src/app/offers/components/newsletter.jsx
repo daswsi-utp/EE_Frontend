5e5de7dd0da3e114a1b7b42bd9d0acce41ebc823
roke741 <jhordieroque741@gmail.com>
2025-04-19 01:12:31 -0500

    style: update Newsletter component styles for improved aesthetics

--- src/app/offers/components/newsletter.jsx
+++ src/app/offers/components/newsletter.jsx
@@ -1,6 +1,6 @@
 export default function Newsletter() {
   return (
-    <section className="bg-teal-800/70 rounded-2xl p-8 text-white">
+    <section className="bg-teal-500 rounded-3xl p-8 text-white mb-16">
       <div className="max-w-3xl mx-auto text-center">
         <h2 className="text-2xl md:text-3xl font-bold mb-4">
           Obtenga ofertas ecológicas exclusivas
@@ -13,7 +13,7 @@ export default function Newsletter() {
           <input
             type="email"
             placeholder="Ingrese su correo electrónico"
-            className="px-4 py-2 rounded-lg flex-1 text-white focus:outline-none focus:ring-2 focus:ring-teal-500"
+            className="px-4 py-2 rounded-lg flex-1 text-white focus:outline-none focus:ring-2 focus:ring-white"
           />
           <button className="bg-teal-400 text-white py-2 px-4 rounded-lg hover:bg-teal-600 transition cursor-pointer">
             Subcribirse
