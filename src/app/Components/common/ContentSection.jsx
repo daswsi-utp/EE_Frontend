52c635b1e1d14c7d9eae30fde49df415d40abb38
Luis Alatta <newluisalattago@gmail.com>
2025-04-22 01:43:14 -0500

    creation component ContentSection

---
+++ src/app/Components/common/ContentSection.jsx
@@ -0,0 +1,20 @@
+// components/common/ContentSection.jsx
+import React from 'react';
+
+const ContentSection = ({ 
+  title, 
+  children, 
+  imageLeft = false, 
+  imageComponent = null 
+}) => (
+  <div className="grid md:grid-cols-2 gap-12 items-center mb-20">
+    {imageLeft && <div>{imageComponent}</div>}
+    <div>
+      <h2 className="text-2xl font-semibold mb-4 text-green-600">{title}</h2>
+      <div className="text-lg leading-relaxed text-gray-700">{children}</div>
+    </div>
+    {!imageLeft && <div>{imageComponent}</div>}
+  </div>
+);
+
+export default ContentSection;
