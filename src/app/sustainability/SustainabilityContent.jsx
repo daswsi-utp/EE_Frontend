846da5d10f44c8f75d065bef359abfe70ce1017a
roke741 <jhordieroque741@gmail.com>
2025-04-23 00:31:50 -0500

    feat: reorganize components in SustainabilityContent for improved structure

--- src/app/sustainability/SustainabilityContent.jsx
+++ src/app/sustainability/SustainabilityContent.jsx
@@ -1,15 +1,21 @@
+"use client";
 import Certifications from "./components/certifications";
 import EnvironmentalImpact from "./components/environmental-impact";
 import HeroSustainability from "./components/hero-sustainability";
+import MaterialSuppliers from "./components/material-suppliers";
 import ProductCarbonFootprint from "./components/product-carbon-footprint";
+import SustainableTips from "./components/sustainable-tips";
+
 
 export default function SustainabilityContent() {
   return (
     <div className="flex flex-col min-h-screen">
       <HeroSustainability />
-      <ProductCarbonFootprint />
-      <Certifications />
       <EnvironmentalImpact />
+      <Certifications />
+      <ProductCarbonFootprint />
+      <MaterialSuppliers />
+      <SustainableTips />
     </div>
   );
 }
