6bb369cdf1ca9262bbba8abf1813fa412bd88404
Marlon Diego <105674967+MarlonLupaca@users.noreply.github.com>
2025-07-03 01:47:09 -0500

    refactor(us): update Timeline component

--- src/app/us/timeline/Timeline.jsx
+++ src/app/us/timeline/Timeline.jsx
@@ -3,8 +3,6 @@ import TimelineItem from './TimelineItem';
 
 const Timeline = ({ events }) => (
   <div className="mb-24 relative">
-    <h2 className="text-2xl font-semibold mb-10 text-center text-primary">Nuestra Evolución</h2>
-
     <div className="absolute top-1/2 left-0 w-full border-t border-gray-300 z-0" />
 
     <div className="overflow-x-auto">
